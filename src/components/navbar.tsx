6ef7894d9b7b9a39d0058f571d0665011a29afa6
RyanGarfinkel <113050972+RyanGarfinkel@users.noreply.github.com>
2024-10-09 11:15:47 -0400

    Switch between light and dark mode

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,13 @@
+import { MoonIcon, SunIcon } from '@radix-ui/react-icons';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-const Navbar = () => {
+interface NavbarProps {
+    isDarkMode: boolean;
+    toggleDarkMode: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ isDarkMode, toggleDarkMode }) => {
 
     const path = usePathname();
 
@@ -11,6 +17,16 @@ const Navbar = () => {
                 { '<RG/>'}
             </div>
             <div className='flex flex-row items-center gap-3 sm:text-sm md:text-lg'>
+                <button onClick={toggleDarkMode}>
+                    {
+                        isDarkMode ? (
+                            <SunIcon/>
+                        ) : (
+                            <MoonIcon/>
+                        )
+                    }
+                </button>
+                <div>|</div>
                 <Link href='/' className={path === '/' ? 'underline underline-offset-4' : ''}>Home</Link>
                 <Link href='/work' className={path === '/work' ? 'underline underline-offset-4' : ''}>Work</Link>
                 <Link href='/projects' className={path === '/projects' ? 'underline underline-offset-4' : ''}>Projects</Link> 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,57 @@
 import Navbar from '@/components/navbar';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { useEffect, useState } from 'react';
 
 interface LayoutProps {
   children: React.ReactNode;
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }) => {
+const Layout: React.FC<LayoutProps> = ({ children, isDarkMode, toggleDarkMode }) => {
 
   return (
     <div className='flex flex-col justify-center mx-auto sm:full lg:w-[750px] sm:px-3 md:px-8 lg:px-0'>
           <title>Ryan Garfinkel</title>
-          <Navbar/>
+          <Navbar isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>
           { children }
       </div>
   );
 };
 
 const App = ({ Component, pageProps }: AppProps) => {
+
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      document.documentElement.classList.add('dark');
+      setIsDarkMode(true);
+    } else {
+      document.documentElement.classList.remove('dark');
+      setIsDarkMode(false);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    if (isDarkMode)
+      {
+      document.documentElement.classList.remove('dark');
+      localStorage.setItem('theme', 'light');
+      setIsDarkMode(false);
+    }
+    else
+    {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('theme', 'dark');
+      setIsDarkMode(true);
+    }
+  };
+
   return (
-    <Layout>
+    <Layout isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}>
       <Component { ...pageProps } />
     </Layout>
   );
